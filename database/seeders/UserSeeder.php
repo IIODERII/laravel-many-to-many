7a224ddc36972703fb785d633d37af5a6aeef49c
IIODERII <oderrisi@gmail.com>
2024-01-19 09:41:54 +0100

    added userSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Oder',
+                'email' => 'o@o.com',
+                'password' => '12345678',
+            ]
+        ];
+        foreach ($users as $user) {
+            $newUser = new User();
+            $newUser->name = $user['name'];
+            $newUser->email = $user['email'];
+            $newUser->password = $user['password'];
+            $newUser->save();
+        }
+    }
+}
